d6b135012f98a6c28df16812803712a182dc4a68
KeenGWatanabe <mckeenweb@gmail.com>
2024-07-07 00:35:39 +0800

    r

--- script/chat.js
+++ script/chat.js
@@ -12,28 +12,90 @@ else if(inputText.toLowerCase().indexOf("thank you")>-1){
 			document.getElementById("answer").innerHTML="You are welcome.不用客气。";}
 
 //cmd ref-basic*
-else if(inputText.toLowerCase().indexOf("list")>-1&&inputText.toLowerCase().indexOf("directory")>-1){document.getElementById("answer").innerHTML=`ls [option]:  List the contents of the 
-current directory.`;}
+else if(inputText.toLowerCase().indexOf("ls [option]")>-1||inputText.toLowerCase().indexOf("list directory")>-1){document.getElementById("answer").innerHTML=` List the contents of the current directory.`;}
 
-else if(inputText.toLowerCase().indexOf("display content")>-1||inputText.toLowerCase().indexOf("content")>-1){document.getElementById("answer").innerHTML=`cat  
-[file-name]: Display the contents of 
-a text file on the screen`;}
+else if(inputText.toLowerCase().indexOf("cat [filename]")>-1||inputText.toLowerCase().indexOf("content")>-1){document.getElementById("answer").innerHTML=`cat [filename]: Display the contents of a text file on the screen`;}
 
-else if(inputText.toLowerCase().indexOf("change directory")>-1||inputText.toLowerCase().indexOf("directory")>-1||inputText.toLowerCase().indexOf("another")>-1&&inputText.toLowerCase().indexOf("directory")>-1){document.getElementById("answer").innerHTML=`cd /etc:  Change from one directory to another.`;}
+else if(inputText.toLowerCase().indexOf("cd /etc")>-1||inputText.toLowerCase().indexOf("change directory")>-1||inputText.toLowerCase().indexOf("another")>-1&&inputText.toLowerCase().indexOf("directory")>-1){document.getElementById("answer").innerHTML=`cd /etc:  Change from one directory to another.`;}
 
-else if(inputText.toLowerCase().indexOf("display")>-1&&inputText.toLowerCase().indexOf("current")>-1||inputText.toLowerCase().indexOf("current")>-1&&inputText.toLowerCase().indexOf("directory")>-1){document.getElementById("answer").innerHTML=` pwd: Displays the present working directory.`;}	
+else if(inputText.toLowerCase().indexOf("pwd")>-1||inputText.toLowerCase().indexOf("current")>-1&&inputText.toLowerCase().indexOf("directory")>-1){document.getElementById("answer").innerHTML=` pwd: Displays the present working directory.`;}	
 
-else if(inputText.toLowerCase().indexOf("display")>-1&&inputText.toLowerCase().indexOf("user")>-1||inputText.toLowerCase().indexOf("current")>-1&&inputText.toLowerCase().indexOf("user")>-1){document.getElementById("answer").innerHTML=`whoami: Displays the username of the current user.`;}
+else if(inputText.toLowerCase().indexOf("whoami")>-1||inputText.toLowerCase().indexOf("current")>-1&&inputText.toLowerCase().indexOf("user")>-1){document.getElementById("answer").innerHTML=`whoami: Displays the username of the current user.`;}
 
-else if(inputText.toLowerCase().indexOf("create")>-1&&inputText.toLowerCase().indexOf("file")>-1||inputText.toLowerCase().indexOf("update")>-1&&inputText.toLowerCase().indexOf("file")>-1){document.getElementById("answer").innerHTML=`touch [file-name]: Create a new empty file or update the timestamp on an existing file`;}
+else if(inputText.toLowerCase().indexOf("touch [filename]")>-1||inputText.toLowerCase().indexOf("update")>-1&&inputText.toLowerCase().indexOf("file")>-1){document.getElementById("answer").innerHTML=`touch [file-name]: Create a new empty file or update the timestamp on an existing file`;}
 
 
-else if(inputText.toLowerCase().indexOf("display")>-1&&inputText.toLowerCase().indexOf("manual")>-1||inputText.toLowerCase().indexOf("display help")>-1||inputText.toLowerCase().indexOf("help")>-1){document.getElementById("answer").innerHTML=`man [command]: Display manual, or help, pages for a specific command.`;}
+else if(inputText.toLowerCase().indexOf("man [command]")>-1||inputText.toLowerCase().indexOf("display help")>-1||inputText.toLowerCase().indexOf("help")>-1){document.getElementById("answer").innerHTML=`man [command]: Display manual, or help, pages for a specific command.`;}
 
-else if(inputText.toLowerCase().indexOf("describe")>-1&&inputText.toLowerCase().indexOf("cmd")>-1||inputText.toLowerCase().indexOf("describe")>-1&&inputText.toLowerCase().indexOf("command")>-1){document.getElementById("answer").innerHTML=`whatis [command]:  Provides a brief description of the  
-specified command.`;}
+else if(inputText.toLowerCase().indexOf("whatis [command]")>-1||inputText.toLowerCase().indexOf("describe")>-1&&inputText.toLowerCase().indexOf("command")>-1){document.getElementById("answer").innerHTML=`whatis [command]:  Provides a brief description of the specified command.`;}
 // cmd-basic***
-
+//usr grp cmd
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("usr pw")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("pw settings")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("curr usr")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("sys usr")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("add usr")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("modify usr")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("delete usr")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("acct info")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("create grp")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("modify grp")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("remove grp")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("switch usr")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("delegate privileges")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("authorize usr")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+//usr grp cmd***
+//permissions cmd
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("alter default")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("modify permissions")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("change owner")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("change owner+grp")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("change grp")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("chg grp ownership")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("list attributes")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("chg attributes")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("retrieve ACL")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("chg ACL permission")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+//permissions***
+//file mgt 
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("file metadata")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("file info")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("create links")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("chg dir")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("filesystem")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("create dir")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("copy file")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("move file")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("rename file")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("delete dir")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("remove dir")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("file front")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("file end")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("search strings")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("exec each argument")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("send output")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("search match")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("search path")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("update database")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("complete path")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+//file mgt***
+//author txt files
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("bundle files")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("reduce size zip")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("reduce size")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("compression")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("archive")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+//author txt files***
+//sw mgt
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("pkg Rhel")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("sw pkg Rhel")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("pkg debian")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("sw older debian")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("download from sites")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("download from site2")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+//sw mgt***
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
+else if(inputText.toLowerCase().indexOf("")>-1||inputText.toLowerCase().indexOf("")>-1&&inputText.toLowerCase().indexOf("")>-1){document.getElementById("answer").innerHTML=``;}
 
 //RAMS***
 //*s8-syllabus**
--- script/list.js
+++ script/list.js
@@ -1,12 +1,13 @@
 function displayAnswer(elementId) {
   if (elementId === '') {document.getElementById("text_a").value = "";}
-else if (elementId === 'a1') {document.getElementById("text_a").value = "list directory";}
-else if (elementId === 'a2') {document.getElementById("text_a").value = "display content";}
-else if (elementId === 'a3') {document.getElementById("text_a").value = "change directory";}
-else if (elementId === 'a4') {document.getElementById("text_a").value = "display current directory";}
-else if (elementId === 'a5') {document.getElementById("text_a").value = "current user";}
-else if (elementId === 'a6') {document.getElementById("text_a").value = "create file";}
-else if (elementId === 'a7') {document.getElementById("text_a").value = "display help";}
+else if (elementId === 'a1') {document.getElementById("text_a").value = "ls [option]";}
+else if (elementId === 'a2') {document.getElementById("text_a").value = "cat [filename]";}
+else if (elementId === 'a3') {document.getElementById("text_a").value = "cd /etc";}
+else if (elementId === 'a4') {document.getElementById("text_a").value = "pwd";}
+else if (elementId === 'a5') {document.getElementById("text_a").value = "whoami";}
+else if (elementId === 'a6') {document.getElementById("text_a").value = "touch [filename]";}
+else if (elementId === 'a7') {document.getElementById("text_a").value = "man [command]";}
+else if (elementId === 'a7i') {document.getElementById("text_a").value = "whatis [command]";}
 else if (elementId === 'a8') {document.getElementById("text_a").value = "cmd";}
 else if (elementId === 'a9') {document.getElementById("text_a").value = "";}
 else if (elementId === 'a10') {document.getElementById("text_a").value = "";}
@@ -33,8 +34,129 @@ else if (elementId === 'a30') {document.getElementById("text_a").value = "most l
 else if (elementId === 'a31') {document.getElementById("text_a").value = "inspire the company";}
 else if (elementId === 'a32') {document.getElementById("text_a").value = "problems";}
 else if (elementId === 'a33') {document.getElementById("text_a").value = "virtual office";}
-else if (elementId === 'a34') {document.getElementById("text_a").value = "virtual assistant";}
-
+else if (elementId === 'a34') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a35') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a36') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a37') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a38') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a39') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a40') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a41') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a42') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a43') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a44') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a45') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a46') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a47') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a48') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a49') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a50') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a51') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a52') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a53') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a54') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a55') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a56') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a57') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a58') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a59') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a60') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a61') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a62') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a63') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a64') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a65') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a66') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a67') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a68') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a69') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a70') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a71') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a72') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a73') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a74') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a75') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a76') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a77') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a78') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a79') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a80') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a81') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a82') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a83') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a84') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a85') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a86') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a87') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a88') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a89') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a90') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a91') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a92') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a93') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a94') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a95') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a96') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a97') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a98') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a99') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a100') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a101') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a102') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a103') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a104') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a105') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a106') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a107') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a108') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a109') {document.getElementById("text_a").value = "";}
+else if (elementId === 'a110') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+else if (elementId === '') {document.getElementById("text_a").value = "";}
 
 //else if (elementId === '') {document.getElementById("text_a").value = "";}
 
