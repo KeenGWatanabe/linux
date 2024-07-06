6279e8d678798734493eba0039e55fcb210ba6ee
KeenGWatanabe <mckeenweb@gmail.com>
2024-07-06 11:46:25 +0800

    r

--- list.js
+++ list.js
@@ -24,51 +24,58 @@ else if (elementId === 'disclaimer') {
   alert(`This content is created for trainee and trained lifesaver's references only.  Context of saving methodology is subject to relevant skills, experiences and expertise interpretation, content here are by no means comprehensive enough to be used alone. The proper way to be an effective lifesaver is to undertake a course of training provided by a licensed trainer and certified by a recognised lifesaving authority. (chatbot developer)`);
 }
 else if (elementId === 's1') {
-  window.open(href="list/s2Hygiene.html",'chat AI','resizable,height=300,width=500');
+  window.open(href="list/s1.html",'chat AI','resizable,height=300,width=500');
 
 }
 else if (elementId === 's2') {
-  window.open(href="list/s3Rescue.html",'chat AI','resizable,height=300,width=500');
+  window.open(href="list/s2.html",'chat AI','resizable,height=300,width=500');
 }
 else if (elementId === 's3') {
   window.open(href="list/s3.html",'chat AI','resizable,height=300,width=500');
 }
-else if (elementId === 's4Resuscitation') {
-  window.open(href="list/s4Resuscitation.html",'chat AI','resizable,height=300,width=500');
+else if (elementId === 's4') {
+  window.open(href="list/s4.html",'chat AI','resizable,height=300,width=500');
 }
-else if (elementId === 's4WaterEAR') {
-  window.open(href="list/s4WaterEAR.html",'chat AI','resizable,height=300,width=500');
+else if (elementId === 's5') {
+  window.open(href="list/s5.html",'chat AI','resizable,height=300,width=500');
 }
-else if (elementId === 's5EmergencyCare') {
-  window.open(href="list/s5EmergencyCare.html",'chat AI','resizable,height=300,width=500');
+else if (elementId === 's6') {
+  window.open(href="list/s6.html",'chat AI','resizable,height=300,width=500');
 }
-else if (elementId === 's5SpinalVideo') {
-  window.open(href="list/s5SpinalVid.html",'chat AI','resizable,height=300,width=500');
-}
-
-else if (elementId === 's6CardiovascularSys') {
-  window.open(href="list/s6CardiovascularSys.html",'chat AI','resizable,height=300,width=500');
-}
-else if (elementId === 's6RespiratoryIntro') {
-  window.open(href="list/s6RespiratorySys.html",'chat AI','resizable,height=300,width=500');
+else if (elementId === 's7') {
+  window.open(href="list/s7.html",'chat AI','resizable,height=300,width=500');
 }
 
-else if (elementId === 's6Spinal') {
-  window.open(href="list/s6Spinal.html",'chat AI','resizable,height=300,width=500');
+else if (elementId === 's8') {
+  window.open(href="list/s8.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's9') {
+  window.open(href="list/s9.html",'chat AI','resizable,height=300,width=500');
+}
+
+else if (elementId === 's10') {
+  window.open(href="list/s10.html",'chat AI','resizable,height=300,width=500');
 
 }
-else if (elementId === 's7Legal') {
-  window.open(href="list/s7Legality.html",'chat AI','resizable,height=300,width=500');
+else if (elementId === 's11') {
+  window.open(href="list/s11.html",'chat AI','resizable,height=300,width=500');
 
 }
-else if (elementId === 's8Rescue') {
-  window.open(href="list/s8Rescue.html",'chat AI','resizable,height=300,width=500');
+else if (elementId === 's12') {
+  window.open(href="list/s12.html",'chat AI','resizable,height=300,width=500');
 }
-else if (elementId === 's8Resuscitation') {
-  window.open(href="list/s8Resuscitation.html",'chat AI','resizable,height=300,width=500');
+else if (elementId === 's13') {
+  window.open(href="list/s13.html",'chat AI','resizable,height=300,width=500');
 }    
-
-
+else if (elementId === 's14') {
+  window.open(href="list/s14.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's15') {
+  window.open(href="list/s15.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's16') {
+  window.open(href="list/s16.html",'chat AI','resizable,height=300,width=500');
+}
 
 
 }
